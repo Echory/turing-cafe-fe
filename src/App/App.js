27ffc29cb92262c5119f65df718828dfe06d0e9e
Emma Chory <emmachory@gmail.com>
2022-02-16 09:48:38 -0700

    Create reservations card

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import {getAllReservations} from '../apiCalls'
+import Card from '../Components/Card'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reservations: []
+    }
+  }
+
+componentDidMount = () => {
+  getAllReservations()
+}
+
+
   render() {
     return (
       <div className="App">
---
+++ src/Components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react' 
+
+const Card = ({id, name, date, time, number}) => {
+  return (
+    <section key="id">
+      <h1>{name}</h1>
+      <p>{date}</p>
+      <p>{time}</p>
+      <p>Table for {number}</p>
+      <p></p>
+    </section>
+  )
+}
+
+export default Card
